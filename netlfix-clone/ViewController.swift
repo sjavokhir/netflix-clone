9808dee0d3c097621ede9a1d2d3d1c8874e75e0f
Javokhir <javokhir@gmail.com>
2022-11-07 15:38:49 +0500

    Initial Commit

---
+++ netlfix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netlfix-clone
+//
+//  Created by Javokhir Savriev on 07/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
